bf76aa59d642fce930a2d12b5ef5018d4ec07b9c
OscarSofka <107581543+OscarSofka@users.noreply.github.com>
2022-07-22 10:06:14 -0500

    Update ReactiveExample.java

--- src/main/java/challenge/ReactiveExample.java
+++ src/main/java/challenge/ReactiveExample.java
@@ -3,31 +3,37 @@ package challenge;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-
+import java.util.Comparator;
+import java.util.List;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 
 public class ReactiveExample {
 
     public static final int VALOR_PERMITIDO = 15;
-    private  Flux<Estudiante> estudianteList;
+    public static final double PUNTAJE_PARA_APROBAR = 75;
+    private Flux<Estudiante> estudianteList;
 
     public ReactiveExample() {
         //TODO: convertir los estudiantes a un Flux
 
-       /* estudianteList = List.of(
-                new Estudiante("raul", 30, List.of(1, 2, 1, 4, 5)),
-                new Estudiante("andres", 35, List.of(4, 2, 4, 3, 5)),
-                new Estudiante("juan", 75, List.of(3, 2, 4, 5, 5)),
-                new Estudiante("pedro", 80, List.of(5, 5, 4, 5, 5)),
-                new Estudiante("santiago", 40, List.of(4, 5, 4, 5, 5))
+        estudianteList = Flux.fromIterable(
+                List.of(
+                        new Estudiante("raul", 30, List.of(1, 2, 1, 4, 5)),
+                        new Estudiante("andres", 35, List.of(4, 2, 4, 3, 5)),
+                        new Estudiante("juan", 75, List.of(3, 2, 4, 5, 5)),
+                        new Estudiante("pedro", 80, List.of(5, 5, 4, 5, 5)),
+                        new Estudiante("santiago", 40, List.of(4, 5, 4, 5, 5))
+                )
         );
-*/
     }
 
     //TODO: suma de puntajes
     public Mono<Integer> sumaDePuntajes() {
-        return null;
+        return estudianteList.map(mapeoDeEstudianteAPuntaje())
+                .reduce(0, Integer::sum);
     }
 
     private Function<Estudiante, Integer> mapeoDeEstudianteAPuntaje() {
@@ -36,36 +42,51 @@ public class ReactiveExample {
 
     //TODO: mayor puntaje de estudiante
     public Flux<Estudiante> mayorPuntajeDeEstudiante(int limit) {
-        return null;
-
+        return estudianteList.sort(Comparator.reverseOrder())
+                .take(limit);
     }
 
-    //TODO: total de asisntencias de estudiantes con mayor puntaje basado en un  valor
-    public Mono<Integer> totalDeAsisntenciasDeEstudiantesConMayorPuntajeDe(int valor) {
-        return null;
+    //TODO: total de asistencias de estudiantes con mayor puntaje basado en un  valor
+    public Mono<Integer> totalDeAsistenciasDeEstudiantesConMayorPuntajeDe(int valor) {
+        return estudianteList.filter(estudiante -> estudiante.getPuntaje() > valor)
+                .flatMapIterable(Estudiante::getAsistencias)
+                .reduce(Integer::sum);
     }
 
     //TODO: el estudiante tiene asistencias correctas
     public Mono<Boolean> elEstudianteTieneAsistenciasCorrectas(Estudiante estudiante) {
-        return null;
+        return Mono.just(estudiante)
+                .filter(asistenciasPemitidas())
+                .hasElement();
+    }
+
+    private Predicate<Estudiante> asistenciasPemitidas() {
+        return estudiante -> estudiante.getAsistencias()
+                .stream()
+                .reduce(0, Integer::sum) >= VALOR_PERMITIDO;
     }
 
     //TODO: promedio de puntajes por estudiantes
     public Mono<Double> promedioDePuntajesPorEstudiantes() {
-        return null;
+        return estudianteList.collect(
+                Collectors.averagingInt(Estudiante::getPuntaje)
+        );
     }
 
 
     //TODO: los nombres de estudiante con puntaje mayor a un valor
     public Flux<String> losNombresDeEstudianteConPuntajeMayorA(int valor) {
-        return null;
+        return estudianteList
+                .filter(estudiante -> estudiante.getPuntaje() > valor)
+                .map(Estudiante::getNombre);
     }
 
-
-
     //TODO: estudiantes aprovados
-    public Flux<String> estudiantesAprovados(){
-        return null;
+    public Flux<String> estudiantesAprobados() {
+        return estudianteList.filter(estudiante -> estudiante.getPuntaje() >= PUNTAJE_PARA_APROBAR)
+                .doOnNext(estudiante -> estudiante.setAprobado(Boolean.TRUE))
+                .filter(Estudiante::isAprobado)
+                .map(Estudiante::getNombre);
     }
 
 
